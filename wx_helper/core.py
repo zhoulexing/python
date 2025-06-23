4217e43f80995c7167c1e94816e47837412e4ce0
zhoulexing <zhoulexing@youzan.com>
2025-06-23 14:28:01 +0800

    fix: 更新获取方式

--- wx_helper/core.py
+++ wx_helper/core.py
@@ -6,10 +6,10 @@ class WxHelperCore:
     def __init__(self):
         self.wx_info = get_wx_info(WX_OFFS)
         print(f"wx_info: {self.wx_info}")
-        self.db_path = merge_db(self.wx_info.wx_dir)
+        self.db_path = merge_db(self.wx_info[0]['wx_dir'])
         print(f"db_path: {self.db_path}")
         output_path = os.path.join(os.path.dirname(__file__), "assets", "wx_db")
-        decrypt_merge_result = decrypt_merge(self.wx_info.wx_dir, self.wx_info.key, output_path)
+        decrypt_merge_result = decrypt_merge(self.wx_info[0]['wx_dir'], self.wx_info[0]['key'], output_path)
         print(f"decrypt_merge_result: {decrypt_merge_result}")
 
 if __name__ == "__main__":
